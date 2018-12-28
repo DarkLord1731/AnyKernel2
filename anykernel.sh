4bc0c1e3f334d3ec1f25f736c79221cb7dca78c8
DarkLord1731 <siddhantnaik17@gmail.com>
2018-12-28 16:28:24 +0530

    Add fixes for Samsung TW/OneUI based ROMs

--- anykernel.sh
+++ anykernel.sh
@@ -42,6 +42,31 @@ replace_file init.spectrum.rc 644 init.spectrum.rc;
 replace_file init.spectrum.sh 755 init.spectrum.sh;
 insert_line init.samsungexynos7870.rc "import init.spectrum.rc" after "import init.fac.rc" "import init.spectrum.rc";
 
+ui_print "- Disabling some useless samsung stuff";
+append_file default.prop "" props;
+
+mount -o rw,remount -t auto /system >/dev/null;
+rm -rf /system/bin/vaultkeeper;
+rm -rf /system/etc/tima;
+rm -rf /system/etc/init/secure_storage_daemon.rc;
+rm -rf /system/lib/libvkjni.so;
+rm -rf /system/lib/libvkservice.so;
+rm -rf /system/lib64/libvkjni.so;
+rm -rf /system/lib64/libvkservice.so;
+rm -rf /system/priv-app/KLMSAgent;
+rm -rf /system/priv-app/KnoxGuard;
+rm -rf /system/priv-app/Rlc;
+rm -rf /system/priv-app/TeeService;
+rm -rf /system/vendor/app/mcRegistry/ffffffffd0000000000000000000000a.tlbin;
+
+# Fix secure storage (Wi-Fi)
+cp /tmp/anykernel/tools/libsecure_storage.so /system/vendor/lib/libsecure_storage.so;
+chmod 644 /system/vendor/lib/libsecure_storage.so;
+cp /tmp/anykernel/tools/libsecure_storage_jni.so /system/vendor/lib/libsecure_storage_jni.so;
+chmod 644 /system/vendor/lib/libsecure_storage_jni.so;
+
+umount /system;
+
 ui_print "- Installing new boot image";
 
 write_boot;
