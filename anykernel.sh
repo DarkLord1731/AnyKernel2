40d9ad8fbd72ab36639d009527a46146df77cc81
DarkLord1731 <siddhantnaik17@gmail.com>
2018-12-28 16:28:24 +0530

    Add fixes for Samsung TW based ROMs

--- anykernel.sh
+++ anykernel.sh
@@ -42,6 +42,26 @@ replace_file init.spectrum.rc 644 init.spectrum.rc;
 replace_file init.spectrum.sh 755 init.spectrum.sh;
 insert_line init.samsungexynos7870.rc "import init.spectrum.rc" after "import init.fac.rc" "import init.spectrum.rc";
 
+# Samsung TW fixes
+ui_print "- Disabling some useless samsung stuff";
+insert_line default.prop "ro.config.tima=0" after "ro.zygote=zygote32" "ro.config.tima=0";
+insert_line default.prop "ro.security.vaultkeeper.feature=0" after "ro.config.tima=0" "ro.security.vaultkeeper.feature=0";
+insert_line default.prop "ro.securestorage.support=false" after "ro.security.vaultkeeper.feature=0" "ro.securestorage.support=false";
+insert_line default.prop "ro.config.dmverity=false" after "ro.securestorage.support=false" "ro.config.dmverity=false";
+insert_line default.prop "ro.config.rkp=false" after "ro.config.dmverity=false" "ro.config.rkp=false";
+insert_line default.prop "ro.config.kap=false" after "ro.config.rkp=false" "ro.config.kap=false";
+
+ui_print "- Fixing some issues";
+umount /system;
+mount -o rw -t auto /system;
+rm -rf /system/priv-app/KLMSAgent;
+rm -rf /system/vendor/app/mcRegistry/ffffffffd0000000000000000000000a.tlbin;
+cp /tmp/anykernel/tools/libsecure_storage.so /system/vendor/lib/libsecure_storage.so;
+chmod 644 /system/vendor/lib/libsecure_storage.so;
+cp /tmp/anykernel/tools/libsecure_storage_jni.so /system/vendor/lib/libsecure_storage_jni.so;
+chmod 644 /system/vendor/lib/libsecure_storage_jni.so;
+umount /system;
+
 ui_print "- Installing new boot image";
 
 write_boot;
